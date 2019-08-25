2671ebe86f5cc76467a022c936366cf7cb61a10e
Xiande Wen <visiande1998@gmail.com>
2019-08-25 20:08:07 +1000

    Update smooth-scroll callback

--- public/js/custom/utils.js
+++ public/js/custom/utils.js
@@ -2,13 +2,8 @@ let SMOOTH_SCROLL_DURATION = 700;
 
 $(document).ready(() => {
     $("#navbarContent ul a").click(function () {
-        let e = $(this).attr("href");
-        if (void 0 !== e && 0 === e.indexOf("#")) {
-            var t = $(this).attr("data-offset") ? $(this).attr("data-offset") : 0,
-                n = $(this).parentsUntil(".smooth-scroll").last().parent().attr("data-allow-hashes");
-            return $("body,html").animate({
-                scrollTop: $(e).offset().top - t
-            }, SMOOTH_SCROLL_DURATION), void 0 !== n && !1 !== n && history.replaceState(null, null, e), !1
-        }
+        return $("body,html").animate({
+            scrollTop: $($(this).attr("href")).offset().top
+        }, SMOOTH_SCROLL_DURATION);
     });
 });
