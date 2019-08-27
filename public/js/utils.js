42719f0aa31cfa554e07fed8794e9b2398acc875
Xiande Wen <visiande1998@gmail.com>
2019-08-27 22:26:20 +1000

    Minor fix

--- public/js/utils.js
+++ public/js/utils.js
@@ -2,15 +2,16 @@ let SMOOTH_SCROLL_DURATION = 700;
 
 $(document).ready(() => {
     $("#navbarContent ul a").click(function () {
-        return $("body,html").animate({
-            scrollTop: $($(this).attr("href")).offset().top
-        }, SMOOTH_SCROLL_DURATION);
+        let o = $($(this).attr("href")).offset();
+        return o ? $("body,html").animate({
+            scrollTop: o.top
+        }, SMOOTH_SCROLL_DURATION) : true;
     });
 });
 
 new WOW().init();
 
 window.onbeforeunload = function () {
-  window.scrollTo(0, 0);
-}
+    window.scrollTo(0, 0);
+};
 
