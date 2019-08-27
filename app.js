74e069a4513216af169f9a44e3dc5acc2965ded4
Xiande Wen <visiande1998@gmail.com>
2019-08-27 12:16:43 +1000

    Add error page

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-// var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+// var favicon = require('serve-favicon');
 
 var routes = require('./routes/routes');
 
@@ -22,33 +23,18 @@ app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+    next(createError(404));
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
+// error handler
 app.use(function (err, req, res) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
     res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+    res.render('error');
 });
 
 
