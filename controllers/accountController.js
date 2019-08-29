278ef205ca4e3a00179ef3df2f45926b9fb7eb84
yaeba <xuankentay@gmail.com>
2019-08-29 12:36:59 +1000

    add send verification email

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 let Account = mongoose.model('Account');
+let Token = mongoose.model('Token');
 
 let emailAvailable = function (req, res) {
     // check if email has already been taken
@@ -10,6 +11,23 @@ let emailAvailable = function (req, res) {
     });
 };
 
+
+/* Signup */
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+let transporter = nodemailer.createTransport({
+    service: "Sendgrid",
+    auth: {
+        user: process.env.SENDGRID_USER,
+        pass: process.env.SENDGRID_PASS
+    }
+});
+let mailOptions = {
+    from: "no-reply@femo.io",
+    subject: "Account Verification Token"
+};
+let crypto = require('crypto');
+
 let signupPost = function (req, res) {
     // create and save new account
     new_acc = new Account({
@@ -18,7 +36,31 @@ let signupPost = function (req, res) {
     });
 
     new_acc.save(function (err) {
-        return res.send(err);
+        // create a new token for this account
+        let token = new Token({
+            _accountId: new_acc._id,
+            token: crypto.randomBytes(16).toString('hex')
+        });
+
+        // save verification token
+        token.save(function(err) {
+            if (err) {
+                console.log("Cannot save token");
+                return res.status(500).send({msg: err.message});
+            }
+            console.log("Token saved");
+
+            mailOptions.to = new_acc.email;
+            mailOptions.text = `Hello, ${new_acc.email}\n\nPlease verify your account by visiting the link: \n
+                    http://${req.headers.host}/confirm-email/${token.token}\n
+                    Note: Be sure to check your spam folder as well!`;
+
+            // send confirmation email
+            transporter.sendMail(mailOptions, function(err) {
+                console.log(`Verification email sent to ${new_acc.email}`);
+                return res.send(err);
+            });
+        });
     });
 };
 
