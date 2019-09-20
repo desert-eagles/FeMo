c1e7e4538cfd941f10e98cfd9014a8c23ad3d12c
Xiande Wen <visiande1998@gmail.com>
2019-09-20 17:34:56 +1000

    Add description, occurredAt and errorModal to upload page

--- public/js/upload/upload.js
+++ public/js/upload/upload.js
@@ -2,9 +2,9 @@ Dropzone.autoDiscover = false;
 
 $(function () {
 
-    let myDropzone = new Dropzone('div#imageUpload', {
+    let myDropzone = new Dropzone('div#media', {
         url: '/upload',
-        maxFilesize: 9,
+        maxFiles: 9,
         addRemoveLinks: true,
         acceptedFiles: "image/*",
         autoProcessQueue: false,
@@ -14,17 +14,30 @@ $(function () {
 
         init: function () {
             // Update selector to match your button
-            $("#uploaderBtn").click(function (e) {
+            $("#post #save").click(function (e) {
                 e.preventDefault();
                 $(".dropzone .dz-preview .dz-progress").show();
-                // TODO validate text inputs
-                myDropzone.processQueue();
+
+                if (myDropzone.getQueuedFiles().length > 0) {
+                    myDropzone.processQueue();
+                } else if ($("#description").val()) {
+                    $.ajax({
+                        url: "/upload",
+                        method: "POST",
+                        data: {
+                            "description": $("#description").val(),
+                            "occurredAt": $("#occurredAt").val()
+                        }
+                    });
+                } else {
+                    $("#errorModal").modal('show');
+                }
                 return false;
             });
 
-            this.on('sending', function (file, xhr, formData) {
-                // TODO append all form inputs to the formData Dropzone will POST
-                formData.append("testname", "test");
+            this.on('sending', function (file, xhr, fd) {
+                fd.append("description", $("#description").val());
+                fd.append("occurredAt", $("#occurredAt").val());
             });
 
             this.on('addedfile', function () {
