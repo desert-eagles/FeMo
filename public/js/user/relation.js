ae3a145ff8ca547a71070fc6de01e30fc27dfa8f
Xiande Wen <visiande1998@gmail.com>
2019-10-12 13:16:31 +1100

    Update relation page

--- public/js/user/relation.js
+++ public/js/user/relation.js
@@ -10,7 +10,8 @@ $(() => {
 });
 
 const searchResultTpl =
-    "<div class='card testimonial-card mb-3 result-card'>" +
+    "<div class='col-lg-3 col-md-4 col-sm-6'>" +
+    "<div class='card testimonial-card my-5 px-3'>" +
 
     // Avatar
     "<div class='avatar mx-auto white'>" +
@@ -18,15 +19,16 @@ const searchResultTpl =
     "</div>" +
 
     // Content
-    "<div class='card-body closed'>" +
+    "<div class='card-body px-0 py-4'>" +
 
     // Name
     "<h5 class='card-title font-weight-bold'>Martha Smith</h5>" +
+
     "<hr>" +
 
     "<div class='md-form select-wrapper mdb-select'>" +
     "<i class='fas fa-user-friends prefix'></i>" +
-    "<label class='mdb-main-label' for='{{resultId}}'>Relation</label>" +
+    "<label class='mdb-main-label' for='{{resultId}}'>Relationship</label>" +
     "<span class='caret'>▼</span>" +
     "<input class='select-dropdown form-control' id='{{resultId}}' type='text' readonly='true' required='false' value='' />" +
     "<ul class='dropdown-content w-100'>" +
@@ -37,6 +39,7 @@ const searchResultTpl =
 
     "<button class='btn btn-sm btn-primary'>Send request</button>" +
 
+    "</div>" +
     "</div>";
 
 function search() {
@@ -49,15 +52,17 @@ function search() {
             type: string.includes("@") ? "email" : "nickname"
         },
         beforeSend: (() => {
-            // $(".spinner-grow").show();
-        }),
-        complate: (() => {
-            $(".spinner-grow").hide();
+            $("#results").empty().append($(
+                "<div class='mx-auto'>" +
+                "<div class='spinner-grow my-5' role='status'></div>" +
+                "</div>"
+            ));
         })
     });
 
     $(searchResultTpl).appendTo($("#results")).hide()
         .imagesLoaded((imgLoad) => {
+            $("#results :visible").remove();
             addSelectEvents($(imgLoad.elements).fadeIn().find(".select-wrapper input"));
         });
 }
